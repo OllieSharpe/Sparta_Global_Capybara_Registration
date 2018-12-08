d6fe6b7beb9bb6bdbda3325191554c52d0b6c641
Keerat <klalia@spartaglobal.com>
2018-12-08 16:13:21 +0000

    Tested middle fields statically

--- lib/pages/registration_page.rb
+++ lib/pages/registration_page.rb
@@ -1,11 +1,74 @@
 require 'Capybara/dsl'
 
 class Registration
+
   include Capybara::DSL
 
+  ADDRESS = 'inputAddress'
+  ADDRESS2 = 'inputAddress2'
+  CITY = 'inputCity'
+  POSTCODE = 'inputPostcode'
+  SKILLS = 'exampleFormControlTextarea1'
+  COUNTY = 'inputCounty'
 
   def visit_registration_page(url)
     visit(url)
+  end
+
+  def input_address
+    fill_in(ADDRESS, :with => '19 North Park')
+  end
+
+  def get_address_value
+    find_field(ADDRESS).value
+  end
+
+  def input_optional_address
+    fill_in(ADDRESS2, :with => '2 Albany Road')
+  end
+
+  def get_address2_value
+    find_field(ADDRESS2).value
+  end
+
+  def input_city
+    fill_in(CITY, :with => 'London')
+  end
+
+  def get_city_value
+    find_field(CITY).value
+  end
+
+  def select_county
+    select "Bristol", :from => "inputCounty"
+  end
+
+  def get_county_value
+    find_field(COUNTY).text.include?('Bristol')
+  end
+
+  def input_postcode
+    fill_in(POSTCODE, :with => 'SL0 9DH')
+  end
+
+  def get_postcode_value
+    find_field(POSTCODE).value
+  end
+
+  def input_email
+    find("input[type='email']").set "k@hotmail.com"
+  end
+
+  def get_email_value
+    find("input[type='email']").value
+  end
+
+  def input_skills
+    fill_in(SKILLS, :with => 'Interpersonal, Problem solving, Dancing')
+  end
+
+  def get_skills_value
+    find_field(SKILLS).value
   end
 
 end
--- spec/registration_form_spec.rb
+++ spec/registration_form_spec.rb
@@ -9,31 +9,39 @@ describe "Testing the registration form for Sparta Global" do
   end
 
   it "should be able to input the address in the address field" do
-    pending
+    @sparta_site.registration_page.input_address
+    expect(@sparta_site.registration_page.get_address_value).to eq '19 North Park'
   end
 
   it "should be able to input the optional address 2 in the address 2 field" do
-    pending
+    @sparta_site.registration_page.input_optional_address
+    expect(@sparta_site.registration_page.get_address2_value).to eq '2 Albany Road'
   end
 
   it "should be able to input a city in the city field" do
-    pending
+    @sparta_site.registration_page.input_city
+    expect(@sparta_site.registration_page.get_city_value).to eq 'London'
   end
 
   it "should be able to select a county from the dropdown menu" do
-    pending
+    @sparta_site.registration_page.select_county
+    expect(@sparta_site.registration_page.get_county_value).to be true
   end
 
   it "should be able to input a postcode in the postcode field" do
-    pending
+    @sparta_site.registration_page.input_postcode
+    expect(@sparta_site.registration_page.get_postcode_value).to eq 'SL0 9DH'
   end
 
   it "should be able to input an email in the email field" do
-    pending
+    @sparta_site.registration_page.input_email
+    expect(@sparta_site.registration_page.get_email_value).to eq 'k@hotmail.com'
   end
 
   it "should be able to input skills in the text area" do
-    pending
+    @sparta_site.registration_page.input_skills
+    expect(@sparta_site.registration_page.get_skills_value).to eq 'Interpersonal, Problem solving, Dancing'
+    sleep 2
   end
 
 end
