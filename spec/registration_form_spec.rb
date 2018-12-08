1e25e818b0fca127f5187d9f4735b0e620b05271
Keerat <klalia@spartaglobal.com>
2018-12-08 13:54:12 +0000

    Updated starter code for the middle fields

--- spec/registration_form_spec.rb
+++ spec/registration_form_spec.rb
@@ -8,8 +8,32 @@ describe "Testing the registration form for Sparta Global" do
     @sparta_site.registration_page.visit_registration_page(@url.to_s)
   end
 
-  it "" do
-  
+  it "should be able to input the address in the address field" do
+    pending
+  end
+
+  it "should be able to input the optional address 2 in the address 2 field" do
+    pending
+  end
+
+  it "should be able to input a city in the city field" do
+    pending
+  end
+
+  it "should be able to select a county from the dropdown menu" do
+    pending
+  end
+
+  it "should be able to input a postcode in the postcode field" do
+    pending
+  end
+
+  it "should be able to input an email in the email field" do
+    pending
+  end
+
+  it "should be able to input skills in the text area" do
+    pending
   end
 
 end
