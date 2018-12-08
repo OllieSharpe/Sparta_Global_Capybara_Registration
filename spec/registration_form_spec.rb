cea1293c04a62eaf789789cd7b272b1364f7c803
Ruben Pinto <rpinto@spartaglobal.com>
2018-12-08 15:59:01 +0000

    Ruben testing framework created

--- spec/registration_form_spec.rb
+++ spec/registration_form_spec.rb
@@ -8,8 +8,33 @@ describe "Testing the registration form for Sparta Global" do
     @sparta_site.registration_page.visit_registration_page(@url.to_s)
   end
 
-  it "" do
-  
+  it "should land on registration page" do
+    pending
   end
+
+  it "should be 11 characters long and accept only numbers" do
+    pending
+  end
+
+  it "should accept a web link" do
+
+  end
+
+  it "should upload cv document" do
+
+  end
+
+  it "should select either SDET or DEVOPS for stream" do
+
+  end
+
+  it "should have terms and conditions selected" do 
+
+  end
+
+  it 'should accept an integer between 1 and 100 inclusive' do
+
+  end
+
 
 end
