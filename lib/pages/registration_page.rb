1e34c6f3e64a1a6fa11cb45cf3207360ac5c986d
Ruben Pinto <rpinto@spartaglobal.com>
2018-12-08 19:07:38 +0000

    method and test added for landing on the registration page, and should accept only number and should be 11 characters

--- lib/pages/registration_page.rb
+++ lib/pages/registration_page.rb
@@ -3,9 +3,39 @@ require 'Capybara/dsl'
 class Registration
   include Capybara::DSL
 
+  def initialize
+    @phone_number = "07778881234"
+  end
+
+  PHONE_NUMBER_INPUT_TYPE = 'tel'
+  LINKEDIN_INPUT_TYPE = 'url'
 
   def visit_registration_page(url)
     visit(url)
   end
+
+  # obtains the current url of the registration page
+  def current_url
+    page.current_url
+  end
+
+  # inputs a phone number in the phone number field
+  def input_phone_number
+    find("input[type='#{PHONE_NUMBER_INPUT_TYPE}']").set("#{@phone_number}")
+  end
+
+  # obtains the id of the phone number input field
+  def get_phone_number_field
+    find("input[type='#{PHONE_NUMBER_INPUT_TYPE}']").value
+  end
+
+  # inputs the linkedin url
+  # def input_linkedin
+  #   find("input[type='#{LINKEDIN_INPUT_TYPE}']")
+  # end
+
+  # def get_linkedin_field
+
+  # end
 
 end
--- spec/registration_form_spec.rb
+++ spec/registration_form_spec.rb
@@ -5,35 +5,38 @@ describe "Testing the registration form for Sparta Global" do
   before(:all) do
     @url = ENV['REGISTRATION_URL']
     @sparta_site = SpartaSite.new
-    @sparta_site.registration_page.visit_registration_page(@url.to_s)
+    @registration_page = @sparta_site.registration_page
   end
 
   it "should land on registration page" do
-    pending
+    @registration_page.visit_registration_page(@url.to_s)
+    expect(@registration_page.current_url).to eq @url
   end
 
   it "should be 11 characters long and accept only numbers" do
-    pending
+    @registration_page.input_phone_number
+    expect(@registration_page.get_phone_number_field.to_i).to be_a_kind_of(Integer)
+    expect(@registration_page.get_phone_number_field.length).to eq 11
   end
 
   it "should accept a web link" do
-
+    pending
   end
 
   it "should upload cv document" do
-
+    pending
   end
 
   it "should select either SDET or DEVOPS for stream" do
-
+    pending
   end
 
   it "should have terms and conditions selected" do 
-
+    pending
   end
 
   it 'should accept an integer between 1 and 100 inclusive' do
-
+    pending
   end
 
 
