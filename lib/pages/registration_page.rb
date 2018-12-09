806fa70d1191b021094dd8e522e69d34a3834f71
Oliver Sharpe <osharpe@spartaglobal.com>
2018-12-09 17:22:21 +0000

    tests complete

--- lib/pages/registration_page.rb
+++ lib/pages/registration_page.rb
@@ -3,9 +3,103 @@ require 'Capybara/dsl'
 class Registration
   include Capybara::DSL
 
+  attr_reader :first_name, :last_name, :age, :date_of_birth, :university, :degree, :university_locator
+
+  def initialize
+    @first_name = 'The Nefarious'
+    @last_name = 'Mr. Dark'
+    @age = "1000"
+    @date_of_birth = '25121018'
+    @university = 'University of Oxford'
+    @degree = 'PhD Dark Lord Sorceries'
+  end
+
+  FIRST_NAME_ID = 'firstName'
+  LAST_NAME_ID = 'lastName'
+  FIRST_NAME_LOCATOR = 'input[id="firstName"]'
+  LAST_NAME_LOCATOR = 'input[id=lastName]'
+  AGE_LOCATOR = 'input[type="number"]'
+  DOB_LOCATOR = 'input[name="dob"]'
+  MALE_GENDER_LOCATOR = 'label[for="customRadioInline1"]'
+  FEMALE_GENDER_LOCATOR = 'label[for="customRadioInline2"]'
+  MALE_GENDER_ID = 'input[id="customRadioInline1"]'
+  FEMALE_GENDER_ID = 'input[id="customRadioInline2"]'
+  DEGREE_LOCATOR = 'input[placeholder="Enter Degree"]'
+  UNIVERSITY_DROPDOWN_ID = '#inputUni'
+  UNIVERSITY_LOCATOR = 'inputUni'
 
   def visit_registration_page(url)
     visit(url)
+  end
+
+  def enter_first_name
+    fill_in(FIRST_NAME_ID, :with => @first_name)
+  end
+
+  def get_first_name
+    find(FIRST_NAME_LOCATOR)['value']
+  end
+
+  def enter_last_name
+    fill_in(LAST_NAME_ID, :with => @last_name)
+  end
+
+  def get_last_name
+    find(LAST_NAME_LOCATOR)['value']
+  end
+
+  def enter_age
+    find(AGE_LOCATOR).send_keys(@age)
+  end
+
+  def get_age
+    find(AGE_LOCATOR)['value']
+  end
+
+  def select_male
+    find(MALE_GENDER_LOCATOR).click
+  end
+
+  def male_selected?
+    find(MALE_GENDER_ID).selected?
+  end
+
+  def select_female
+    find(FEMALE_GENDER_LOCATOR).click
+  end
+
+  def female_selected?
+    find(FEMALE_GENDER_ID).selected?
+  end
+
+  def enter_date_of_birth
+    find(DOB_LOCATOR).send_keys(@date_of_birth)
+  end
+
+  def get_date_of_birth
+    find(DOB_LOCATOR)['value']
+  end
+
+  def enter_degree
+    find(DEGREE_LOCATOR).send_keys(@degree)
+  end
+
+  def get_degree
+    find(DEGREE_LOCATOR)['value']
+  end
+
+  def select_university
+    select(@university, :from => UNIVERSITY_LOCATOR)
+  end
+
+  def get_university
+    find(UNIVERSITY_DROPDOWN_ID).value
+  end
+
+  def convert_date(date)
+    date_array = date.split('-')
+    date_array.reverse!
+    date_array.join
   end
 
 end
--- spec/registration_form_spec.rb
+++ spec/registration_form_spec.rb
@@ -9,31 +9,42 @@ describe "Testing the registration form for Sparta Global" do
   end
 
   it "Should accept a given first name" do
-
+    @sparta_site.registration_page.enter_first_name
+    expect(@sparta_site.registration_page.get_first_name).to eq @sparta_site.registration_page.first_name
   end
 
   it "Should accept a given last name" do
-
+    @sparta_site.registration_page.enter_last_name
+    expect(@sparta_site.registration_page.get_last_name).to eq @sparta_site.registration_page.last_name
   end
 
   it "Should accept a given age" do
-
+    @sparta_site.registration_page.enter_age
+    expect(@sparta_site.registration_page.get_age).to eq @sparta_site.registration_page.age
   end
 
   it "Should accept a given date of birth" do
-
+    @sparta_site.registration_page.enter_date_of_birth
+    expect(@sparta_site.registration_page.convert_date(@sparta_site.registration_page.get_date_of_birth)).to eq @sparta_site.registration_page.date_of_birth
   end
 
   it "Should accept one of either male or female" do
-
+    @sparta_site.registration_page.select_female
+    expect(@sparta_site.registration_page.female_selected?).to eq true
+    @sparta_site.registration_page.select_male
+    expect(@sparta_site.registration_page.male_selected?).to eq true
+    expect(@sparta_site.registration_page.female_selected?).to eq false
   end
 
   it "Should accept a degree title" do
-
+    @sparta_site.registration_page.enter_degree
+    expect(@sparta_site.registration_page.get_degree).to eq @sparta_site.registration_page.degree
   end
 
   it "Should accept select the correct university from the dropdown box" do
-
+    @sparta_site.registration_page.select_university
+    sleep 2
+    expect(@sparta_site.registration_page.get_university).to eq @sparta_site.registration_page.university
   end
 
 end
