9de05601bd1cb0d7b816705a30b3011d16b830f9
Oliver Sharpe <osharpe@spartaglobal.com>
2018-12-09 14:50:00 +0000

    framework for the first 7 tests

--- spec/registration_form_spec.rb
+++ spec/registration_form_spec.rb
@@ -8,8 +8,32 @@ describe "Testing the registration form for Sparta Global" do
     @sparta_site.registration_page.visit_registration_page(@url.to_s)
   end
 
-  it "" do
-  
+  it "Should accept a given first name" do
+
+  end
+
+  it "Should accept a given last name" do
+
+  end
+
+  it "Should accept a given age" do
+
+  end
+
+  it "Should accept a given date of birth" do
+
+  end
+
+  it "Should accept one of either male or female" do
+
+  end
+
+  it "Should accept a degree title" do
+
+  end
+
+  it "Should accept select the correct university from the dropdown box" do
+
   end
 
 end
